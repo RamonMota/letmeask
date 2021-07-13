9a32b686f950785c0c77166349b80ccccb240efc
Ramon Mota <65029645+RamonMota@users.noreply.github.com>
2021-07-13 17:40:28 -0300

    Update Room.tsx
    new room page

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,33 @@
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button'
+
+
 export function Room() {
     return(
-        <h1>Sala</h1>
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="Letmeask"/>
+                    <div>code</div>
+                </div>
+            </header>
+            <main className="content">
+                <div className="room-title">
+                    <h1>React Room</h1>
+                    <span>4 question</span>
+                </div>
+
+                <form>
+                    <textarea 
+                    placeholder="What is your question?"
+                    />
+
+                    <div className="form-footer">
+                        <span>TO invite a question, <button>Sing in</button>.</span>
+                        <Button type="submit">Send question</Button>
+                    </div>
+                </form>
+            </main>
+        </div>
     )
 }
